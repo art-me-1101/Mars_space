fe5ba867b5e56ed19a571ba4ce6ef664436fa6c1
art-me-1011 <krutov.artm@yandex.ru>
2024-02-17 14:01:05 +0300

    task3

--- lesson 2/main.py
+++ lesson 2/main.py
@@ -15,5 +15,14 @@ def training(prof):
     return render_template('base2.html', profession=name, url=img)
 
 
+@app.route("/list_prof/<string:type>")
+def list_prof(type):
+    prof_list = ['инженер-исследователь', ' пилот', ' строитель', ' экзобиолог', ' врач',
+                 ' инженер по терраформированию', ' климатолог', ' специалист по радиационной защите', ' астрогеолог',
+                 ' гляциолог', ' инженер жизнеобеспечения', ' метеоролог', ' оператор марсохода', ' киберинженер',
+                 ' штурман', ' пилот дронов']
+    return render_template('base3.html', type=type, prof_list=prof_list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host="127.0.0.1")
